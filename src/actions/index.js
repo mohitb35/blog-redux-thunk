2b6f99359a74a4198cdf0249c66a9ccace2154a8
Mohit Bajaj <mohit_b35@hotmail.com>
2020-05-02 11:48:05 +0530

    add react-thunk middleware to handle async actions - use applyMiddleware function (from redux) while creating store - refactor fetchPosts action creator to return an async function which: a. accepts dispatch function as an argument b. calls dispatch with action after fetching data

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,13 +2,28 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 // Action creators
 
-export const fetchPosts = async () => {
-	// Bad code
+/* export const fetchPosts = () => {
+	return async function(dispatch, getState) {
+		const response = await jsonPlaceholder.get('/posts');
+
+		// Dispatching action
+		dispatch ({
+			type: 'FETCH_POSTS',
+			payload: response
+		});
+	}
+}; */ //See refactored code below
+
+
+// Refactored -- removed getState argument (unnecessary), 
+// parenthesis around returned function arguments as it has only one argument
+// parenthesis around main function as it consists of a single return statement
+export const fetchPosts = () => async dispatch =>  {
 	const response = await jsonPlaceholder.get('/posts');
 
-	// Returning action
-	return {
+	// Dispatching action
+	dispatch ({
 		type: 'FETCH_POSTS',
 		payload: response
-	};
+	});
 };
